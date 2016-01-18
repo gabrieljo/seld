d3b31e7a199fea85d9605fa6d108df85da93fcb9
gabriel <gabrieljo1302@gmail.com>
2016-01-19 10:40:30 +1300

    delet all

--- application/views/m_design/s3-settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo form_open("u/create/design", array('class'=>'form-horizontal'));
-
-if (isset($type)){
-	include($page."-{$type}.php");
-}
-
-echo form_close();
-?>
--- files/js/s3-settings.js
+++
@@ -1,5 +0,0 @@
-var step = {
-	init: function(){
-		
-	}
-};
